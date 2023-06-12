0ee4b4647e3abefd672fd956e42eccc7e23fffdb
Rshimmering <chen_jer@163.com>
2023-06-12 15:46:46 +0800

    Site updated: 2023-06-12 15:46:23

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "#548e87",
+    "tmpSize": "16",
+    "cityColor": "#548e87",
+    "citySize": "16",
+    "aqiColor": "#865858",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "15px 10px 10px 20px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "e63661998ba1418b8e8a149f21255ed2"
+  }
+}
